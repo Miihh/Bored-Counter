e559b5d8617f86a28ba915c0f39ed5b48f240b11
Miihh <69526529+Miihh@users.noreply.github.com>
2021-04-26 18:57:48 +0300

    buttons working

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ var buttons = document.querySelectorAll(".btn");
 var value = document.querySelector("#value");
 
 
+
+function clicked(event) {
+    var action = event.currentTarget.classList;
+    if(action.contains('decrease')) {
+        count--;
+    } else if (action.contains('increase')) {
+        count++;
+    }else {
+        count = 0;
+    }
+    return value.textContent =count;
+}
+
 buttons.forEach(function(btn) {
     btn.addEventListener('click', clicked);
     } )
-
-
-function clicked(clicked) {
-    console.log(clicked.currentTarget.classList);
-}
