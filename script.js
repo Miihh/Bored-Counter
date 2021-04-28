1b36c889e628aa1ecf55abb88de37df482b0f017
Miihh <69526529+Miihh@users.noreply.github.com>
2021-04-28 11:33:51 +0300

    Added CSS & JS. Garfield is gone

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function clicked(event) {
 buttons.forEach(function(btn) {
     btn.addEventListener('click', clicked);
     } )
+
+
+//BORED JS PART
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
